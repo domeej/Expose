bb62663a7fe75676584603f9c32246c7f2b1c8e2
domeej <vyruza@freenet.de>
2018-06-05 23:43:50 +0200

    first models, bootstrap files

--- karma/models.py
+++ karma/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Alias(models.Model):
+    Player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+
+
+class Game(models.Model):
+    date = models.DateTimeField('date of the Game')
+
+
+
