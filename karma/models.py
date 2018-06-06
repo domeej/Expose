c2d5e6ca748f66e05e05865e994ad0a37d334dbc
domeej <vyruza@freenet.de>
2018-06-06 16:03:27 +0200

    bootstrap shizzl

--- karma/models.py
+++ karma/models.py
@@ -8,12 +8,12 @@ class Player(models.Model):
 
 
 class Alias(models.Model):
-    Player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    alias_name = models.CharField(max_length=20)
 
 
 class Game(models.Model):
-    date = models.DateTimeField('date of the Game')
+    game_date = models.DateTimeField('date of the Game')
 
 
 
